ce28ef5c88d8de61b1c461dad13f564e4ef52a71
Pedro Andriotti <pedroandriotti@MacBook-Air-de-Pedro-2.local>
2023-09-22 18:49:44 -0300

    Parallel mode working

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ void enqueue(struct Node **head, struct Node **tail, char *command);
 void printList(struct Node *head);
 void freeList(struct Node *head);
 void sequencialShell(char *line);
-// void parallelShell(char *command);
+void parallelShell(char *line);
 
 int main(void) {
     struct Node *head = NULL;
@@ -30,9 +30,11 @@ int main(void) {
         if(isParallel == 0){
             printf("phab seq>");
         }
+
         if(isParallel == 1){
             printf("phab par>");
         }
+
         read = getline(&line, &len, stdin);
 
         line[strcspn(line, "\n")] = '\0';
@@ -47,25 +49,34 @@ int main(void) {
             isParallel = 1;
             continue;        
         }
+
         if(strcmp(line, "style sequencial") == 0){
             free(line);
             isParallel = 0;
             continue;        
         }
+
+        if(strcmp(line, "!!") == 0){
+            free(line);
+            if (head == NULL) {
+                printf("No commands.\n");
+                continue;
+            }
+            line = strdup(tail->command);
+            printf("%s\n", line);
+        }
+
         if(isParallel){
             enqueue(&head, &tail, line);
             printList(head);
 
-            // parallelShell(line);
+            parallelShell(line);
         }else{
             enqueue(&head, &tail, line);
             printList(head);
 
             sequencialShell(line);
-
         }
-
-
     }
 
     freeList(head);
@@ -139,28 +150,31 @@ void sequencialShell(char *line) {
     }
 }
 
-// void parallelShell(char *line){
-//     char *args[40];
-//     int i = 0;
+void parallelShell(char *line)  {
+    char *commands[40];
+    int commandsqty = 1;
+    int i = 0;
 
-//     char *token = strtok(line, " \n");
+    char *token = strtok(line, ";");
 
-//     while (token != NULL) {
-//         args[i] = token;
-//         token = strtok(NULL, " \n");
-//         i++;
-//     }
+    while(token != NULL){
+        commands[commandsqty] = strdup(token);
+        token = strtok(NULL, ";");
+        commandsqty++;
+    }
 
-//     args[i] = NULL;
+    for (int i = 0; i < commandsqty; i++) {
+        pid_t pid = fork();
+        if (pid == 0) { 
+        char *cmd = commands[i];
+        system(cmd);
+        exit(EXIT_SUCCESS);
+        }
+    }
+    
+    for (int i = 0; i < commandsqty; i++) {
+    int status;
+    wait(&status);
+    }
 
-//     if (i > 0) {
-//         pid_t pid = fork();
-
-//         if (pid < 0) {
-//             fprintf(stderr, "Fork Failed\n");
-//         } else if (pid == 0) {
-//             execvp(args[0], args);
-//             exit(1);
-//         }
-//     }
-// }
+}
